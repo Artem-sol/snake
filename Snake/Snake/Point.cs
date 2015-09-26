88bee34dfda99875d867fddabf6fd157274b81c8
Artem-sol <artek492005@mail.ru>
2015-09-26 16:54:17 +0300

    INC

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -8,9 +8,17 @@ namespace Snake
 {
     class Point
     {
-        public int x;
-        public int y;
-        public char sym;
+        int x;
+        int y;
+        char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+
+        }
 
         public void Draw()
         {
